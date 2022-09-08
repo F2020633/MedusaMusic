64458dd2569f838c5e3bbe747ce20b43fc649bf0
F2020633 <76026491+F2020633@users.noreply.github.com>
2022-09-08 20:59:11 +0400

    Update config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 
 
-API_ID = os.getenv('API_ID',12345)                            # You can get this from -->  https://my.telegram.org/auth  
-API_HASH = os.getenv('API_HASH','You API HASH')               # You can get this from -->  https://my.telegram.org/auth
-BOT_TOKEN = os.getenv('BOT_TOKEN','Bot token here')           # You can get this from --> @BotFather
-BOT_NAME = os.getenv('BOT_NAME','MedusaMousikibot')           #Username of your bot
-GENIUS_API = os.getenv('GENIUS_API','genius api here')        # You can get this from --> https://docs.genius.com/
+API_ID = os.getenv('API_ID',12394287)                            # You can get this from -->  https://my.telegram.org/auth  
+API_HASH = os.getenv('API_HASH','adf9d0f1457cbe17c24576432b38c476')               # You can get this from -->  https://my.telegram.org/auth
+BOT_TOKEN = os.getenv('BOT_TOKEN','5779646393:AAG6T_WiIOb-bfRcO4Ya8Paxuop9QvhzwQM')           # You can get this from --> @BotFather
+BOT_NAME = os.getenv('BOT_NAME','DKB_Song_Bot')           #Username of your bot
+GENIUS_API = os.getenv('GENIUS_API','DCGE7zLDmbr5P_bnvaqKjzTm11qy-7yISbxoHs0ksjkT0zqUnTKPv6Y8u84IYtd9')        # You can get this from --> https://docs.genius.com/
